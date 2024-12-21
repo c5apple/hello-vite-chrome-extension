9762a64e9070d07f53981fc2600df0a51c4d2e5b
c5apple <c5apple_party@yahoo.co.jp>
2024-12-22 01:40:52 +0900

    vite

--- dist/popup.js
+++
@@ -1,7 +0,0 @@
-import { HELLO } from './constants';
-const $body = document.querySelector('body');
-const $p = document.createElement('p');
-$p.innerHTML = `${HELLO} Popup`;
-if ($body) {
-    $body.appendChild($p);
-}
